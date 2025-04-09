3b8950a4c9a19f0b5e59bb30ee6cf20d04f8f8e1
Virginia <vfuentesr01@educarex.es>
2025-04-09 18:05:49 +0200

    Prueba web

---
+++ app/routes/home.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Estamos probando la página web" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return
+  <div>
+      <h1 className="text-4xl">HOLA :)</h1>
+      <h2>Esto es un subtítulo</h2>
+   </div>
+}
+//return <<welcome>
