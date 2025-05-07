07da1b49e3c742027fc0555ef03897b41e8161cc
Virginia <vfuentesr01@educarex.es>
2025-05-07 18:22:41 +0200

    07.05C

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import Ejemplo from "~/components/Ejemplo";
 import type { Route } from "./+types/home";
 import Noticia from "~/components/noticia";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Estamos probando la página web" },
     { name: "description", content: "Welcome to React Router!" },
@@ -10,25 +10,51 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+  return (
   
-  return(
-    <div>
-      <h1 className="text-4xl">HOLA :)</h1>
-      <h2>Esto es un subtítulo</h2>
-      <div>
-        <h1>Noticias</h1>
-        <Ejemplo/>
-        {/* Noticias */}
-        <section className="border border-yellow-300">
-          {/* Noticias1 */}
-          <Noticia titulo="Título 1" descripcion="Descripción 1" gap={8}/>
-           {/* Noticias2 */}
-          <Noticia titulo="Título 2" descripcion="Descripción 2" />
-           {/* Noticias3 */}
-          <Noticia titulo= "Título 3" descripcion="Descripción 3"/>
-        </section>
+    <div className="flex h-screen m-auto w-full items-center justify-center">
+    <div className="grid h-full w-full gap-4 bg-gray-200 p-2 grid-cols-4 grid-rows-4 rounded-lg shadow-md">
+    
+      <div 
+        className="col-span-2 row-span-1 bg-pink-200 rounded-lg shadow-md flex items-center justify-center"
+      >
+        <p>Salmon</p>
       </div>
-   </div>
-   )
+    
+      <div 
+        className="col-span-2 row-span-1 bg-lime-200 rounded-lg shadow-md flex items-center justify-center"
+      >
+        <p>Broccoli</p>
+      </div>
+    
+      <div 
+        className="col-span-1 row-span-4 bg-yellow-200 rounded-lg shadow-md flex items-center justify-center"
+      >
+        <p>Tamago</p>
+      </div>
+    
+      <div 
+        className="col-span-2 row-span-2 bg-tan-200 rounded-lg shadow-md flex items-center justify-center"
+      >
+        <p>Pork</p>
+      </div>
+    
+      <div 
+        className="col-span-1 row-span-2 bg-green-200 rounded-lg shadow-md flex items-center justify-center"
+      >
+        <p>Edamame</p>
+      </div>
+    
+      <div 
+        className="col-span-3 row-span-2 bg-red-200 rounded-lg shadow-md flex items-center justify-center"
+      >
+        <p>Tomato</p>
+      </div>
+    
+    </div>
+  </div>
+  
+  )
 }
 //return <<welcome>
